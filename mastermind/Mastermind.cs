306cb759ff7d71ba3c3d93d41929e41ab8af65f9
Lanzhou Jiang <lzjiang99@gmail.com>
2020-11-30 10:59:33 +1100

    refactored production code , green bar

--- mastermind/Mastermind.cs
+++ mastermind/Mastermind.cs
@@ -22,32 +22,35 @@ namespace mastermind
 
         public KeyPeg[] CreateHintBasedOnPlayerSolution(Peg[] solution)
         {
+            var hint = CreateHintWith4EmptyKeyPegs();
+
+            if (solution == null) return hint;
+            if (SecretSolution == null) return hint;
+
+            for (var i = 0; i < solution.Length; i++)
+            {
+                foreach (var item in SecretSolution)
+                {
+                    if (solution[i].Colour == item.Colour)
+                    {
+                        hint[i].Value = Value.White;
+                    }
+                    if (solution[i].Colour == SecretSolution[i].Colour)
+                    {
+                        hint[i].Value = Value.Black;
+                    }
+                }
+            }
+            return hint;
+        }
+
+        private static KeyPeg[] CreateHintWith4EmptyKeyPegs()
+        {
             var keyPeg1 = new KeyPeg(Value.Empty);
             var keyPeg2 = new KeyPeg(Value.Empty);
             var keyPeg3 = new KeyPeg(Value.Empty);
             var keyPeg4 = new KeyPeg(Value.Empty);
             var hint = new[] {keyPeg1, keyPeg2, keyPeg3, keyPeg4};
-            
-            if (solution == null) return hint;
-            if (SecretSolution == null) return hint;
-
-            for (int i = 0; i < solution.Length; i++)
-            {
-                if (solution[i].Colour == SecretSolution[i].Colour)
-                {
-                    hint[i].Value = Value.Black;
-                }
-                else
-                {
-                    foreach (var item in SecretSolution)
-                    {
-                        if (solution[i].Colour == item.Colour)
-                        {
-                            hint[i].Value = Value.White;
-                        }
-                    }
-                }
-            }
             return hint;
         }
     }
